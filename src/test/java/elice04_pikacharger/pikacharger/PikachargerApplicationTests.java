a44e52bcc836c387cab461f9cc92d6d06d15c379
silversilk <gdmk68@gmail.com>
2024-04-14 22:03:58 +0900

    first_commit

---
+++ src/test/java/elice04_pikacharger/pikacharger/PikachargerApplicationTests.java
@@ -0,0 +1,13 @@
+package elice04_pikacharger.pikacharger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikachargerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

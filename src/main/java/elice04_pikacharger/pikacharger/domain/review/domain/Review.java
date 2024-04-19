d339afb0712a2854ca48b05df913b3bbd9d94358
silversilk <gdmk68@gmail.com>
2024-04-19 21:59:10 +0900

    Fix : Review entity, Repository 수정

--- src/main/java/elice04_pikacharger/pikacharger/domain/review/domain/Review.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/review/domain/Review.java
@@ -1,7 +1,8 @@
 package elice04_pikacharger.pikacharger.domain.review.domain;
 
 import elice04_pikacharger.pikacharger.domain.common.BaseEntity;
-import elice04_pikacharger.pikacharger.domain.image.domain.Image;
+import elice04_pikacharger.pikacharger.domain.image.domain.ReviewImage;
+import elice04_pikacharger.pikacharger.domain.review.dto.payload.ReviewModifyPayload;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.catalina.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -28,19 +30,25 @@ public class Review extends BaseEntity{
 
     @ManyToOne
     @JoinColumn(name = "user_id") //user가 삭제되면 review도 함께 삭제
-    private User user;
+    private User userId;
 
     @ManyToOne
     @JoinColumn(name = "used_charger_id") //charger가 삭제되면 review도 함께 삭제
     private Charger usedCharger;
 
     @OneToMany(mappedBy = "review", cascade = CascadeType.REMOVE) //review 삭제시 관련 image도 함께 삭제
-    private List<Image> review_image;
+    private List<ReviewImage> review_image = new ArrayList<>();
 
     @Builder
     public Review(String content, Integer rating, String scope){
         this.content = content;
         this.rating = rating;
         this.scope = scope;
+    }
+
+    public Long update(ReviewModifyPayload reviewDTO){
+        this.content = reviewDTO.getContent();
+        this.rating = reviewDTO.getRating();
+        return this.id;
     }
 }

4d6eae9893c7fd13c9c8b9627097711425e5ee02
silversilk <gdmk68@gmail.com>
2024-04-25 14:58:13 +0900

    fix : mapper, image 관련 dto 수정

--- src/main/java/elice04_pikacharger/pikacharger/domain/common/mapper/ReviewMapper.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/common/mapper/ReviewMapper.java
@@ -2,9 +2,10 @@ package elice04_pikacharger.pikacharger.domain.common.mapper;
 
 import elice04_pikacharger.pikacharger.domain.review.domain.Review;
 import elice04_pikacharger.pikacharger.domain.review.dto.result.ReviewResult;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-//해당 방식과 @Mapper 어노테이션을 이용한 MyBatis를 사용할지 고민중!
-@Component //빈 등록
-public interface ReviewMapper extends EntityMapper<ReviewResult, Review> {
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ReviewMapper extends EntityMapper{
+    ReviewResult toDto(Review review);
 }

5d5ceb89c2a2e7285099142d4cb146fd589d522a
mean-g <82minique@gmail.com>
2024-04-26 00:29:47 +0900

    feat : chatroom 구현중 / 오류 주석처리

---
+++ src/main/java/elice04_pikacharger/pikacharger/domain/chat/controller/ChatRoomController.java
@@ -0,0 +1,9 @@
+package elice04_pikacharger.pikacharger.domain.chat.controller;
+
+import elice04_pikacharger.pikacharger.domain.chat.service.ChatRoomService;
+import elice04_pikacharger.pikacharger.security.JwtProvider;
+
+public class ChatRoomController {
+//    private final ChatRoomService chatRoomService;
+//    private final JwtProvider jwtProvider;
+}
--- src/main/java/elice04_pikacharger/pikacharger/domain/chat/entity/ChatRoom.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/chat/entity/ChatRoom.java
@@ -1,16 +1,15 @@
-package elice04_pikacharger.pikacharger.domain.chatroom;
+package elice04_pikacharger.pikacharger.domain.chat.entity;
 
 import elice04_pikacharger.pikacharger.domain.common.BaseEntity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Table(name="chatroom")
 @Entity
-@Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ChatRoom extends BaseEntity {
     @Id
@@ -18,8 +17,10 @@ public class ChatRoom extends BaseEntity {
     @Column(name = "chatroom_id")
     private Long id;
 
-    // 수정필요
-
+    @Builder
+    public ChatRoom(Long id) {
+        this.id = id;
+    }
     // 채팅은 삭제x 채팅방은 삭제가능하게하는데, 유저 삭제를 원하는 유저한테서만 삭제되고, 원하지 않던 상대방은 남아있어야함
     // ->가능하면 수정하고 FE에 말해주기
 }

247f26b84bf721161ba14574cc88aaf618ad67ea
dbdudann <dbdudann@gmail.com>
2024-04-30 21:11:12 +0900

    feat: 충전소 swagger 정보 추가

--- src/main/java/elice04_pikacharger/pikacharger/domain/charger/controller/ChargerController.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/charger/controller/ChargerController.java
@@ -5,6 +5,8 @@ import elice04_pikacharger.pikacharger.domain.charger.dto.payload.ChargerCreateD
 import elice04_pikacharger.pikacharger.domain.charger.dto.payload.ChargerUpdateDto;
 import elice04_pikacharger.pikacharger.domain.charger.dto.payload.LocationNameDto;
 import elice04_pikacharger.pikacharger.domain.charger.service.ChargerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,40 +19,47 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chargers")
+@Tag(name = "(충전소)", description = "충전소 관련 api")
 public class ChargerController {
 
     private final ChargerService chargerService;
 
+    @Operation(summary = "충전소 상세 조회", description = "충전소 id를 이용한 충전소 상세 데이터 반환")
     @GetMapping("/{chargerId}")
     public ResponseEntity<ChargerDetailResponseDto> chargerDetail(@PathVariable Long chargerId) {
         ChargerDetailResponseDto chargerDetailResponseDto = chargerService.chargerDetail(chargerId);
         return new ResponseEntity<>(chargerDetailResponseDto, HttpStatus.OK);
     }
 
+    @Operation(summary = "충전소 수정폼 데이터 조회", description = "충전소 수정시 폼안에 들어갈 데이터 반환")
     @GetMapping("/{chargerId}/edit")
     public ResponseEntity<ChargerEditResponseDto> getChargerEditForm(@PathVariable Long chargerId) {
         ChargerEditResponseDto chargerEditResponseDto = chargerService.chargerEditDetail(chargerId);
         return new ResponseEntity<>(chargerEditResponseDto, HttpStatus.OK);
     }
 
+    @Operation(summary = "충전소 검색", description = "도로명, 지번 주소로 검색시 반경 2km 충전소 데이터 반환")
     @GetMapping("")
     public ResponseEntity<List<GroupedChargerResponseDto>> getChargerSearchList(@RequestParam String location) {
         List<GroupedChargerResponseDto> groupedChargerResponseDtoList = chargerService.chargerSearch(location);
         return new ResponseEntity<>(groupedChargerResponseDtoList, HttpStatus.OK);
     }
 
+    @Operation(summary = "충전소 등록", description = "충전소 등록")
     @PostMapping("")
     public ResponseEntity<ChargerResponseDto> createCharger(@Valid @RequestBody ChargerCreateDto chargerCreateDto){
         ChargerResponseDto chargerResponseDto = chargerService.createCharger(chargerCreateDto);
         return new ResponseEntity<>(chargerResponseDto, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "충전소 수정", description = "충전소 id와 유저 id를 이용해 자신이 만든 충전소일 경우 수정")
     @PatchMapping("/{chargerId}/users/{userId}")
     public ResponseEntity<ChargerResponseDto> updateCharger(@Valid @RequestBody ChargerUpdateDto chargerUpdateDto, @PathVariable Long chargerId, @PathVariable Long userId) {
         ChargerResponseDto chargerResponseDto = chargerService.updateCharger(chargerUpdateDto, chargerId, userId);
         return new ResponseEntity<>(chargerResponseDto, HttpStatus.OK);
     }
 
+    @Operation(summary = "충전소 삭제", description = "충전소 id와 유저 id를 이용해 자신이 만든 충전소일 경우 삭제")
     @DeleteMapping("/{chargerId}/users/{userId}")
     public ResponseEntity<Void> deleteCharger(@PathVariable Long chargerId, @PathVariable Long userId) {
         chargerService.deleteCharger(chargerId, userId);

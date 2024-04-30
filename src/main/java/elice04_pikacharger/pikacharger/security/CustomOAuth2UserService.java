5c46183782bfef19c57f44aa1caff671f6dc2426
silversilk <gdmk68@gmail.com>
2024-04-30 12:22:23 +0900

    Fix : 백업

--- src/main/java/elice04_pikacharger/pikacharger/security/CustomOAuth2UserService.java
+++ src/main/java/elice04_pikacharger/pikacharger/security/CustomOAuth2UserService.java
@@ -1,48 +1,48 @@
-package elice04_pikacharger.pikacharger.security;
-
-import elice04_pikacharger.pikacharger.domain.user.entity.User;
-import elice04_pikacharger.pikacharger.domain.user.repository.UserRepository;
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-@RequiredArgsConstructor
-@Service
-public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-
-    private final UserRepository userRepository;
-    private final HttpSession httpSession;
-
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
-        OAuth2User oAuth2User = delegate.loadUser(userRequest);
-
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
-
-        OAuthAttribute attributes = OAuthAttribute.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-
-        User user = save(attributes);
-
-        httpSession.setAttribute("user", new SessionUser(user));
-
-        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRole())),
-                attributes.getAttributes(),
-                attributes.getNameAttributeKey());
-    }
-    private User save(OAuthAttribute attributes){
-        User user = userRepository.findByEmail(attributes.getEmail())
-                .update(attributes.getPicture());
-        return userRepository.save(user);
-    }
-}
-
+//package elice04_pikacharger.pikacharger.security;
+//
+//import elice04_pikacharger.pikacharger.domain.user.entity.User;
+//import elice04_pikacharger.pikacharger.domain.user.repository.UserRepository;
+//import jakarta.servlet.http.HttpSession;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+//import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+//import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+//import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Collections;
+//@RequiredArgsConstructor
+//@Service
+//public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+//
+//    private final UserRepository userRepository;
+//    private final HttpSession httpSession;
+//
+//    @Override
+//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+//        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+//        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+//
+//        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+//        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+//
+//        OAuthAttribute attributes = OAuthAttribute.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+//
+//        User user = save(attributes);
+//
+//        httpSession.setAttribute("user", new SessionUser(user));
+//
+//        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRole())),
+//                attributes.getAttributes(),
+//                attributes.getNameAttributeKey());
+//    }
+//    private User save(OAuthAttribute attributes){
+//        User user = userRepository.findByEmail(attributes.getEmail())
+//                .update(attributes.getPicture());
+//        return userRepository.save(user);
+//    }
+//}
+//
--- src/main/java/elice04_pikacharger/pikacharger/security/OAuthAttribute.java
+++ src/main/java/elice04_pikacharger/pikacharger/security/OAuthAttribute.java
@@ -1,48 +1,48 @@
-package elice04_pikacharger.pikacharger.security;
-
-import elice04_pikacharger.pikacharger.domain.user.entity.User;
-import lombok.Builder;
-import lombok.Getter;
-
-import java.util.Map;
-
-@Getter
-public class OAuthAttribute {
-    private Map<String, Object> attributes;
-    private String nameAttributeKey;
-    private String name;
-    private String email;
-    private String picture;
-
-    @Builder
-    public OAuthAttribute(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
-        this.attributes = attributes;
-        this.nameAttributeKey = nameAttributeKey;
-        this.name = name;
-        this.email = email;
-        this.picture = picture;
-    }
-
-    public static OAuthAttribute of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
-        return ofGoogle(userNameAttributeName,attributes);
-    }
-
-    public static OAuthAttribute ofGoogle(String userNameAttributeName, Map<String,Object> attributes){
-        return OAuthAttribute.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("picture"))
-                .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName)
-                .build();
-    }
-
-    public User toEnity(){
-        return User.builder()
-                .username(name)
-                .email(email)
-                .profileImage(picture)
-                //.role(UserRole)
-                .build();
-    }
-}
+//package elice04_pikacharger.pikacharger.security;
+//
+//import elice04_pikacharger.pikacharger.domain.user.entity.User;
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//import java.util.Map;
+//
+//@Getter
+//public class OAuthAttribute {
+//    private Map<String, Object> attributes;
+//    private String nameAttributeKey;
+//    private String name;/*/
+//    private String email;
+//    private String picture;
+//
+//    @Builder
+//    public OAuthAttribute(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+//        this.attributes = attributes;
+//        this.nameAttributeKey = nameAttributeKey;
+//        this.name = name;
+//        this.email = email;
+//        this.picture = picture;
+//    }
+//
+//    public static OAuthAttribute of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
+//        return ofGoogle(userNameAttributeName,attributes);
+//    }
+//
+//    public static OAuthAttribute ofGoogle(String userNameAttributeName, Map<String,Object> attributes){
+//        return OAuthAttribute.builder()
+//                .name((String) attributes.get("name"))
+//                .email((String) attributes.get("email"))
+//                .picture((String) attributes.get("picture"))
+//                .attributes(attributes)
+//                .nameAttributeKey(userNameAttributeName)
+//                .build();
+//    }
+//
+//    public User toEnity(){
+//        return User.builder()
+//                .username(name)
+//                .email(email)
+//                .profileImage(picture)
+//                //.role(UserRole)
+//                .build();
+//    }
+//}

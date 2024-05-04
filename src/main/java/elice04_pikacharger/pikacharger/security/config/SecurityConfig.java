b26c52d2dc1d01210f4f6b99c64b8f0ff87eaeb7
jooinhyuk <masterhyuk0613@gmail.com>
2024-05-04 23:55:17 +0900

    add cors config in SecurityConfig

--- src/main/java/elice04_pikacharger/pikacharger/security/config/SecurityConfig.java
+++ src/main/java/elice04_pikacharger/pikacharger/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import elice04_pikacharger.pikacharger.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -16,13 +17,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.savedrequest.RequestCacheAwareFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
     private final JwtUtil jwtUtil;
 
     @Bean
@@ -46,5 +49,21 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, RequestCacheAwareFilter.class)
                 .build();
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://ec2-43-203-7-98.ap-northeast-2.compute.amazonaws.com:8080","http://ec2-43-203-7-98.ap-northeast-2.compute.amazonaws.com:3000", "http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.HEAD.name())
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+
 
 }

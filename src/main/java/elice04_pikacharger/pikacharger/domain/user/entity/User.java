1da6d09ca247eecd38e24fa81c16c98224fc0242
jooinhyuk <masterhyuk0613@gmail.com>
2024-05-11 00:23:40 +0900

    유저 연관관계 매핑 수정

--- src/main/java/elice04_pikacharger/pikacharger/domain/user/entity/User.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package elice04_pikacharger.pikacharger.domain.user.entity;
 
 
 import elice04_pikacharger.pikacharger.domain.charger.entity.Charger;
+import elice04_pikacharger.pikacharger.domain.chat.entity.ChatLog;
 import elice04_pikacharger.pikacharger.domain.common.BaseEntity;
 import elice04_pikacharger.pikacharger.domain.favorite.entity.Favorite;
 import elice04_pikacharger.pikacharger.domain.review.domain.Review;
@@ -106,5 +107,15 @@ public class User extends BaseEntity {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Charger> chargers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatLog> chatLogs = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Charger> chatRooms = new ArrayList<>();
+
+    @OneToMany(mappedBy = "receiverId", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Charger> receivers = new ArrayList<>();
+
 
 }

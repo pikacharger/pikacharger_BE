3802fa551990f6376e663f486d8ee68027d10022
mean-g <82minique@gmail.com>
2024-06-02 23:58:15 +0900

    fix: 단일 채팅방 조회 api 생성

--- src/main/java/elice04_pikacharger/pikacharger/domain/chat/controller/ChatRoomController.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/chat/controller/ChatRoomController.java
@@ -2,6 +2,8 @@ package elice04_pikacharger.pikacharger.domain.chat.controller;
 
 import elice04_pikacharger.pikacharger.domain.chat.dto.ChatLogResponseDto;
 import elice04_pikacharger.pikacharger.domain.chat.dto.ChatRoomResponseDto;
+import elice04_pikacharger.pikacharger.domain.chat.dto.CreateChatRoomResponseDto;
+import elice04_pikacharger.pikacharger.domain.chat.dto.SingleChatRoomResponseDto;
 import elice04_pikacharger.pikacharger.domain.chat.service.ChatLogService;
 import elice04_pikacharger.pikacharger.domain.chat.service.ChatRoomService;
 import elice04_pikacharger.pikacharger.security.jwt.JwtUtil;
@@ -19,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,7 +33,21 @@ public class ChatRoomController {
     private final ChatLogService chatLogService;
     private final JwtUtil jwtUtil;
 
-    //채팅방 목록 받기
+    // 단일 채팅방 조회
+    @Operation(summary = "단일 채팅방 조회", description = "요천한 하나의 채팅방을 조회한다", tags = { "Chat" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ChatLogResponseDto.class))))
+    })
+    @GetMapping("/{chatRoomId}")
+    public ApiResult<SingleChatRoomResponseDto> getChatRoom(@PathVariable("chatRoomId") Long chatRoomId) {
+        SingleChatRoomResponseDto chatRoom = chatRoomService.findById(chatRoomId);
+        if (chatRoom == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다.");
+        }
+        return ApiUtils.success(chatRoom);
+    }
+
+    // 채팅방 목록 받기
     @Operation(summary = "채팅방 목록 조회", description = "사용자의 채팅방 목록을 조회한다", tags = { "Chat" })
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ChatRoomResponseDto.class))))
@@ -52,15 +69,14 @@ public class ChatRoomController {
         return ApiUtils.success(chatLogs);
     }
 
-    // 채팅방 생성 (userId, chargerId)
+    // 채팅방 생성
     @Operation(summary = "채팅방 생성", description = "유저가 게시글에 메시지를 보낸다")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ChatRoomResponseDto.class)))
     })
     @PostMapping("")
-    public ResponseEntity<ChatRoomResponseDto> createChatRoom(@RequestParam Long chargerId, @AuthenticationPrincipal Long userId) {
-
-        ChatRoomResponseDto chatroomId = chatRoomService.save(chargerId, userId);
+    public ResponseEntity<CreateChatRoomResponseDto> createChatRoom(@RequestParam Long chargerId, @AuthenticationPrincipal Long userId) {
+        CreateChatRoomResponseDto chatroomId = chatRoomService.save(chargerId, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(chatroomId);
     }
 }

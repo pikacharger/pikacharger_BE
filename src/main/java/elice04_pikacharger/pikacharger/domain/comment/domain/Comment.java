43d6efe6d6b34f81ff1649efe62a713ac63ba717
silversilk <gdmk68@gmail.com>
2024-06-10 16:19:19 +0900

    Fix : Builder 생성자 추가

--- src/main/java/elice04_pikacharger/pikacharger/domain/comment/domain/Comment.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/comment/domain/Comment.java
@@ -1,11 +1,12 @@
 package elice04_pikacharger.pikacharger.domain.comment.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import elice04_pikacharger.pikacharger.domain.common.BaseEntity;
+import elice04_pikacharger.pikacharger.domain.common.global.BaseEntity;
 import elice04_pikacharger.pikacharger.domain.review.domain.Review;
 import elice04_pikacharger.pikacharger.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
@@ -43,6 +44,7 @@ public class Comment extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(name = "parent_id")
+    @JsonBackReference
     private Comment parent;
 
     @OneToMany(mappedBy = "parent", orphanRemoval = true)
@@ -56,7 +58,7 @@ public class Comment extends BaseEntity {
         this.review = review;
     }
 
-    public void updateParent(Comment comment){
+    public void updateParent(Comment parent) {
         this.parent = parent;
     }
 
@@ -64,4 +66,17 @@ public class Comment extends BaseEntity {
         this.isDeleted = isDeleted;
     }
 
+    public void updateContent(String content){
+        this.content = content;
+    }
+
+    // 생성자 없애도 q 클래스에서 protected 에러 발생 안하는지 확인 필요.
+    @Builder
+    public Comment(String content, User user, Review review, Comment parent) {
+        this.content = content;
+        this.user = user;
+        this.review = review;
+        this.parent = parent;
+        this.isDeleted = false;
+    }
 }

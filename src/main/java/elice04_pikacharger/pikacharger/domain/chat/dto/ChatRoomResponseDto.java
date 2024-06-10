9b3250250674931a505bfdfe1931ef2e9507bf18
mean-g <82minique@gmail.com>
2024-06-10 11:48:38 +0900

    fix: 전체채팅방 응답값 수정 (유저정보 추가 요청사항 반영)

--- src/main/java/elice04_pikacharger/pikacharger/domain/chat/dto/ChatRoomResponseDto.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/chat/dto/ChatRoomResponseDto.java
@@ -11,26 +11,46 @@ import java.time.LocalDateTime;
 @Data
 public class ChatRoomResponseDto {
     private Long chatRoomId;
-    private String userImgUrl;
-    private String nickname;
+    private Long userId;
+    private String userProfileImg;
+    private String userNickname;
+    private Long receiverId;
+    private String receiverProfileImg;
+    private String receiverNickname;
     private LocalDateTime createDate;
     private String lastMessage;
 
-    public static ChatRoomResponseDto toEntity(Long chatRoomId, User user, ChatRoom chatRoom, ChatLog lastChatLog) {
+    public static ChatRoomResponseDto toEntity(ChatRoom chatRoom) {
+        ChatLog lastChatLog = chatRoom.getLastChatLog();
+        User user = chatRoom.getUser();
+        User receiver = chatRoom.getReceiverId();
+
+        if (user == null || receiver == null) {
+            throw new IllegalArgumentException("User and Receiver cannot be null");
+        }
+
         return ChatRoomResponseDto.builder()
-                .chatRoomId(chatRoomId)
-                .userImgUrl(user.getProfileImage())
-                .nickname(user.getNickName())
+                .chatRoomId(chatRoom.getId())
+                .userId(user.getId())
+                .userProfileImg(user.getProfileImage())
+                .userNickname(user.getNickName())
+                .receiverId(receiver.getId())
+                .receiverProfileImg(receiver.getProfileImage())
+                .receiverNickname(receiver.getNickName())
                 .createDate(chatRoom.getCreateDate())
                 .lastMessage(lastChatLog != null ? lastChatLog.getMessageContents() : "대화를 기다리고 있어요. 무엇이든 물어보세요!")
                 .build();
     }
 
     @Builder
-    public ChatRoomResponseDto(Long chatRoomId, String userImgUrl, String nickname, LocalDateTime createDate, String lastMessage) {
+    public ChatRoomResponseDto(Long chatRoomId, Long userId, String userProfileImg, String userNickname, LocalDateTime createDate, String lastMessage, Long receiverId, String receiverProfileImg, String receiverNickname) {
         this.chatRoomId = chatRoomId;
-        this.userImgUrl = userImgUrl;
-        this.nickname = nickname;
+        this.userId = userId;
+        this.userProfileImg = userProfileImg;
+        this.userNickname = userNickname;
+        this.receiverId = receiverId;
+        this.receiverProfileImg = receiverProfileImg;
+        this.receiverNickname = receiverNickname;
         this.createDate = createDate;
         this.lastMessage = lastMessage;
     }

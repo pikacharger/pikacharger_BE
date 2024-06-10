354cb89a1d47cbb2e2b3f46cee0d8ee86dd8d6bf
silversilk <gdmk68@gmail.com>
2024-06-10 16:20:01 +0900

    Fix : Comment Mapper, Dto 수정.

--- src/main/java/elice04_pikacharger/pikacharger/domain/comment/mapper/CommentRequestMapper.java
+++ src/main/java/elice04_pikacharger/pikacharger/domain/comment/mapper/CommentRequestMapper.java
@@ -4,9 +4,14 @@ import elice04_pikacharger.pikacharger.domain.comment.domain.Comment;
 import elice04_pikacharger.pikacharger.domain.comment.dto.CommentResultDto;
 import elice04_pikacharger.pikacharger.domain.common.mapper.EntityMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CommentRequestMapper extends EntityMapper<CommentResultDto, Comment> {
     CommentRequestMapper INSTANCE = Mappers.getMapper(CommentRequestMapper.class);
+
+//    @Mapping(target = "content", source = "content")
+//    @Mapping(target = "parent.id", source = "parentId") // 매핑 추가
+//    Comment toEntity(CommentResultDto dto);
 }

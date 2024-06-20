7552e37a4f9ccb44e8597551abc3a2621eb8498e
mean-g <82minique@gmail.com>
2024-06-20 18:09:00 +0900

    fix: websocket allowedOrigins에 localhost추가

--- src/main/java/elice04_pikacharger/pikacharger/websocket/WebSocketConfig.java
+++ src/main/java/elice04_pikacharger/pikacharger/websocket/WebSocketConfig.java
@@ -15,6 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler, "/pikachat/chatroom/{chatroomId}")
-                .setAllowedOrigins("https://pikacharger.store");
+                .setAllowedOrigins("https://pikacharger.store",
+                        "http://localhost");
     }
 }
